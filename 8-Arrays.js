99807ce4d8912948c03a97dd4ec54b2f495178f8
atrubaabid <atrubaabid7@gmail.com>
2026-07-09 13:42:13 -0700

    Done

---
+++ 8-Arrays.js
@@ -0,0 +1,28 @@
+//* ======================================
+//* ARRAYS IN JAVASCRIPT
+//* =====================================
+
+
+
+//* ======================================
+//*  Creating Arrays:
+//* =====================================
+
+
+//? Using Array constructor
+// let fruits = new Array('apple', 'orange', 'banana')
+// console.log(fruits);
+
+
+
+//? Using array literal
+// let fruits2 = ["apple", "orange", "banana"];
+// console.log(fruits2);
+
+
+
+//? we can also create an empty array
+// let arr = [];
+// console.log(typeof arr);
+
+
