827752d820218182aa07c92fa5deba474dd65579
atrubaabid <atrubaabid7@gmail.com>
2026-07-10 04:32:19 -0700

    Done

---
+++ 9-Strings.js
@@ -0,0 +1,40 @@
+//* =========================================
+//* String in JavaScript
+//* =========================================
+
+
+// const str = "Hello,    World!";
+// console.log(str.length);
+
+
+
+
+//* =========================================
+//* Escape Character
+//* =========================================
+
+
+
+//? Escape Character: In JavaScript, the backslash \ is used as an escape character. It allows you to include special characters in a string.
+
+// Code	   Result	    Description
+// \'	    '	        Single quote
+// \"	    "	        Double quote
+// \\	    \	        Backslash
+
+
+
+// let text = "My name is ' Syeda Atruba '  & I am a Full Stack Developer. ";
+// let text = 'My name is " Syeda Atruba  " & I am a Full Stack Developer. ';
+// let text = "My name is ' Syeda Atruba ' & \\ I am a \n \"Full Stack Developer\". ";
+
+// console.log(text);
+
+
+
+//* =========================================
+//* String Search Methods
+//* =========================================
+
+
+
