d61f8d133b8f683593511b8675dde16303dc1609
atrubaabid <atrubaabid7@gmail.com>
2026-07-16 13:48:29 -0700

    Done

--- 15-Date&TimeObj.js
+++ 15-Date&TimeObj.js
@@ -6,12 +6,13 @@
 //* JavaScript Get Date&Time Methods / Getting Components:
 //* ===================================================
 
+// All these methods return Current date and time
 
 // let date = new Date();
 // console.log(date);
 // 1. DATE
 // console.log(date.getFullYear());             //2026
-// console.log(date.getMonth());               //5
+// console.log(date.getMonth());                //5
 // console.log(date.getDate());                //22
 // console.log(date.getDay());                 //1
 // 2. TIME
@@ -26,31 +27,54 @@
 
 
 
+// ===============================================================================================================================================================================
+
 
 //* ==================================================
 //* JavaScript Set Date Methods / Setting Components:
 //* ===================================================
 
+// All these methods return the date and time that I set.
 
 // let date = new Date();
-// console.log(date);
-// // 1. DATE
-// console.log(date.setFullYear(2025));
-// console.log(date);
-// console.log(date.setMonth(5));
-// console.log(date);
-// console.log(date.setDate(11));
-// console.log(date);
-// // 2. TIME
-// console.log(date.setHours(10));
-// console.log(date);
-// console.log(date.setMinutes(9));
-// console.log(date);
-// console.log(date.setSeconds(44));
-// console.log(date);
-// console.log(date.setMilliseconds(200));  
+// console.log(date);                              //Thu Jul 16 2026
+
+// 1. DATE
+// console.log(date.setFullYear(2025));            //1752693180656
+// console.log(date);                              //Wed Jul 16 2025
+
+// console.log(date.setMonth(3));                  //1744831052551
+// console.log(date);                              //Wed Apr 16 2025
+
+// console.log(date.setDate(11));                  //1744399246104
+// console.log(date);                              //Fri Apr 11 2025
+
+
+// we can do that also---------------------------
+
+// let v = date.setDate(12);
+// console.log(new Date(v));
+
+// ==============================================================
+
+// let date = new Date();
+// console.log(date);                              //12:24:07
+
+// // // 2. TIME
+// console.log(date.setHours(10));                 //1784222659354
+// console.log(date);                              //10:24:35
+
+// console.log(date.setMinutes(9));                //1784221758357
+// console.log(date);                              //10:09:29
+
+// console.log(date.setSeconds(44));               //1784221784939
+// console.log(date);                              //10:09:44
+
+// console.log(date.setMilliseconds(200));         //1784221784200
 // console.log(date);
 
+
+// ===============================================================================================================================================================================
 
 
 
@@ -61,43 +85,81 @@
 
 // // 1 => Current date aur current time.
 // let d = new Date();
-// console.log(d);
+// console.log(d);                     //Thu Jul 16 2026 12:28:50
 
-// // 2 => Specific date.
+
+// -----------------------------------------------------
+
+// // 2 => Specific date. ismy year and month same hi rehga but date change hosakti hy bcz of time zone
+
 // let d2 = new Date("2023-01-10");
-// console.log(d2);
+// console.log(d2);                        //Mon Jan 09 2023
 
-// // 3 => String ke through date.
-// let d3 = new Date("July 15, 2026");
-// console.log(d3);
+// -----------------------------------------------------
 
-// // 4 .
+
+// // 3 => String ke through date. ager is formate me date liko tw year month date same hi rahega
+
+
+// let d3 = new Date("Apr 14, 2026");
+// console.log(d3);                        //Tue Apr 14 2026
+
+
+
+// -----------------------------------------------------
+
+
+// // 4  =>  Numbers ke through date. ager is formate me date likho tw year date same rehgi or months index number wise print hoga
+
+
 // let d4 = new Date(2026, 2, 15);
-// console.log(d4);
+// console.log(d4);                        //Sun Mar 15 2026
 
-// // 5 
+
+// -----------------------------------------------------
+
+
+// // 5  =>  Numbers ke through date & Time.
+
 // let d5 = new Date(2026, 6, 15, 10, 30);    //Year Month Date Hours Minutes
-// console.log(d5);
+// console.log(d5);                           //Wed Jul 15 2026 10:30:00
 
-// // 6
+
+// -----------------------------------------------------
+
+
+// // 6  =>   Numbers ke through date & Time + seconds
+
 // let d6 = new Date(2026, 6, 15, 10, 30, 20);  // Second bhi add hogaya
 // console.log(d6);
 
 
-// // 7
+// -----------------------------------------------------
+
+
+// // 7  =>   Numbers ke through date & Time + Milliseconds
+
 // let d7 = new Date(2026, 6, 15, 10, 30, 20, 500);  // Milliseconds bhi add hogaye
 // console.log(d7);
 
+
+
+// -----------------------------------------------------
+
+
 // // 8 => Ye Unix Epoch kehlata hai.
+
 // let d8 = new Date(0);
-// console.log(d8);
-
-// // 9 => Milliseconds ke hisab se date banata hai.
-// let d9 = new Date(1000000000);
-// console.log(d9);
+// console.log(d8);                //Wed Dec 31 1969 16:00:00
 
 
+// -----------------------------------------------------
 
+
+// // 9 => Milliseconds ke hisab se date banata hai. or Convert  Milliseconds into date
+
+// let d9 = new Date(1000000000000);
+// console.log(d9);                        //Sat Sep 08 2001 18:46:40
 
 
 // ====================================================================================================================================================
@@ -116,23 +178,44 @@
 // console.log(date);
 
 
-// ---------------------------------------------------
+// -----------------------------------------------------------------------------------------------------------------------
 
 
 
-//getTime()  =>  Ye 1 January 1970 se lekar us date aur time tak milliseconds return kerta hy.
+// getTime()  =>  Ye 1 January 1970 se lekar us date aur time tak milliseconds return kerta hy.
 
 // 1 January 1970 sy abhi tk jo milliseconds hyn
 // let d = new Date();
-// console.log(d.getTime());
 
-// 1 January 1970 sy is date tk jo milliseconds hyn
-// let date = new Date("July 15, 2026");
-// console.log(date.getTime());
+// // get milliseconds
+// let d2 = d.getTime();           // 1784231940005
+
+// // convert milliseconds into date
+// console.log(new Date(d2));      // Thu Jul 16 2026 12:59:57
+
+
+// -------------------------------------------
+
+
+// // 1 January 1970 sy is date tk jo milliseconds hyn
+// let date = new Date("July 15, 2025");
+// console.log(date);
+
+
+// // // get milliseconds
+// let d2 = date.getTime();           // 1784231940005
+
+// // // convert milliseconds into date
+// console.log(new Date(d2));      // Thu Jul 16 2026 12:59:57
+
+
+// -------------------------------------------
 
 
 
-// Date.now() =>  ko Date object banane ki zarurat nahi hoti.
+
+// Date.now()  =>  ko Date object banane ki zarurat nahi hoti. ye 1970 sy current time tk milliseconds return kerta hy
+
 // getTime()  => use karne ke liye pehle new Date() se Date object banana padta hai.
 
 
@@ -160,7 +243,7 @@
 // toLocaleTimeString()  =>  Time readable form me.
 
 // let date = new Date();
-// console.log(date.toLocaleTimeString());  
+// console.log(date.toLocaleTimeString());
 
 
 // ---------------------------------------------------
@@ -186,8 +269,11 @@
 
 // 1. Do dates ka difference nikalne ke liye
 
-// let start = new Date("2025-01-01");
-// let end = new Date("2025-01-10");
+// let start = new Date("2025-02-02");
+// let end = new Date("2025-02-10");
+// console.log(start);
+// console.log(end);
+
 
 // let difference = end.getTime() - start.getTime();
 
@@ -229,9 +315,9 @@
 
 
 // const addDaysToDate = (date, extraDay) => {
-//     //   console.log(date);
+//     // console.log(date);
 //     // console.log(date.setDate(date.getDate() + extraDay));
-//     //   console.log(new Date(1709769600000));
+//     // console.log(new Date(1709769600000));
 //     let updatedDate = date.setDate(date.getDate() + extraDay);
 //     return new Date(updatedDate);
 // };
@@ -250,16 +336,28 @@
 
 // 5. Write a function to calculate the difference in days between two given dates.
 
+
+
+
 // const getDaysDifference = (d1, d2) => {
-//   let oneDay = 24 * 60 * 60 * 1000;
-//   let diff = Math.abs(d1 - d2);
-//   return Math.round(diff / oneDay);
+//     let diff = Math.abs(d1 - d2);
+//     // console.log(diff);          //950400000    
+
+//     let days = diff / (24 * 60 * 60 * 1000);
+//     // console.log(days);           //11
+
+//     return days.toFixed();
+
 // };
 
-// // Example usage:
+
 // const date1 = new Date("2024-02-19");
 // const date2 = new Date("2024-03-01");
-// console.log(getDaysDifference(date1, date2)); // Output: 11 (difference in days)
+// console.log(getDaysDifference(date1, date2));
+
+
+
+
 
 
 
